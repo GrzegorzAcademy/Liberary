82d2f4791681b8b279d882c9ed2a0d6dfd8cca6c
Grzegorz Nagórski <sparki_21@tlen.pl>
2020-02-26 19:11:38 +0100

    ex1

---
+++ src/Library.java
@@ -0,0 +1,26 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.2";
+
+
+        Book book1 = new Book();
+
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.relaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "9788373271890";
+
+
+
+        System.out.println(appName);
+        System.out.println("Ksiązki w bibliotece");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.relaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
+    }
+}
