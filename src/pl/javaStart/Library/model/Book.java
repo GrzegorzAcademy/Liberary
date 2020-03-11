f1689d5618d746f6177cd1e88e8f6d76125aee21
Grzegorz Nagórski <59772089+GrzegorzAcademy@users.noreply.github.com>
2020-03-11 11:02:49 +0100

    equals

--- src/pl/javaStart/Library/model/Book.java
+++ src/pl/javaStart/Library/model/Book.java
@@ -1,5 +1,7 @@
 package pl.javaStart.Library.model;
 
+import java.util.Objects;
+
 public class Book extends Publication{
 
     private String author ;
@@ -38,16 +40,25 @@ public class Book extends Publication{
         this.isbn = isbn;
     }
 
-    public void printInfo() {
-        String info = getTitle()+" ; "+ author+ " ; "+getYear()+ " ; "+ pages + " ; "+ getPublisher();
+     @Override
+    public String toString() {
+         return super.toString() + author + ", " + pages + ", " + isbn;
+     }
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return pages == book.pages &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(isbn, book.isbn);
+    }
 
-        if(isbn!= null) {
-            info = info+ " ; "+ isbn;
-        }
-        System.out.println(info);
-
-        }
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author, pages, isbn);
+    }
 }
 
 
--- src/pl/javaStart/Library/model/Magazine.java
+++ src/pl/javaStart/Library/model/Magazine.java
@@ -1,5 +1,7 @@
 package pl.javaStart.Library.model;
 
+import java.util.Objects;
+
 public class Magazine extends Publication {
     private int month;
     private int day;
@@ -36,10 +38,24 @@ public class Magazine extends Publication {
     public void setLanguae(String languae) {
         this.languae = languae;
     }
-   @Override
-    public void printInfo(){
-        String info = getTitle()+ "; "+ getPublisher()+ "; "+ getYear()
-                + "- "+ month+ "- "+ day + ", "+ languae;
-        System.out.println(info);
+    @Override
+    public String toString() {
+        return super.toString()+ " "+ month +" "+ day+" "+ languae;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Magazine magazine = (Magazine) o;
+        return month == magazine.month &&
+                day == magazine.day &&
+                Objects.equals(languae, magazine.languae);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), month, day, languae);
     }
 }
